041aeead14670fb9418fd98d5543b0cca16d0518
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2022-08-30 21:53:55 +0200

    Herencia simple sin usar la declaración de clases

--- ejemplos/prototipos.js
+++ ejemplos/prototipos.js
@@ -11,3 +11,18 @@ const luis=new Persona('Luis');
 Persona.prototype.saluda=function(){console.log(`Hola, soy ${this.nombre}`)};
 
 luis.saluda();
+
+//Herencia de Persona -------------------------------
+function Agente(nombre) {
+  //heredamos propiedades, métodos y constructor de Persona
+  //LLamamos al constructor de las personas con mi this y con el nombre dado
+  Persona.call(this, nombre);
+}
+
+//Heredados propiedades y métodos de las personas
+Agente.prototype=Object.create(Persona.prototype);
+Agente.prototype.constructor=Agente;
+
+const brown=new Agente('Brown');
+
+brown.saluda();
