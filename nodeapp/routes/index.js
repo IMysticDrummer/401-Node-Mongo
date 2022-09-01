fda0fa9b13dbfe2755c07eedeade9cf6acd97db9
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2022-09-01 23:49:09 +0200

    Pruebas de parámetros en ruta realizadas

--- nodeapp/routes/index.js
+++ nodeapp/routes/index.js
@@ -25,4 +25,33 @@ router.get('/', function(req, res, next) {
 
 });
 
+//Recibiendo parémetros en la ruta
+router.get('/parametro_en_ruta/:numero', (req, res, next)=>{
+  const numero=req.params.numero;
+  res.send('He recibido '+ numero);
+});
+
+router.get('/parametro_opcional/:talla?', (req, res, next)=>{
+  const laTallaRecibida=req.params.talla;
+  res.send('La talla es '+ laTallaRecibida);
+});
+
+router.get('/piso/:piso([0-9]+)/puerta/:puerta', (req, res, next)=>{
+  console.log(req.params);
+  res.send('Ok');
+});
+
+router.get('/en_query_string', (req, res, next)=>{
+  console.log(req.query);
+  const orderBy=req.query.orderBy;
+  const solo=req.query.solo;
+  res.send('Order por '+orderBy+' y solo: '+solo);
+});
+
+router.put('/', (req, res, next)=>{
+  const color=req.body.color;
+  console.log('Por put he recibido color '+color);
+  res.send('Por put he recibido '+ color);
+});
+
 module.exports = router;
