992a52c1b3d1ce619a0c6fb6853b9a47a0012054
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2022-09-01 23:19:40 +0200

    Pruebas con el generador de vistas ejs hechas

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -13,6 +13,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Integración de variables globales
+app.locals.title="Anuncios"; //Ahora tenemos que adaptarlo en el middleware
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- nodeapp/routes/index.js
+++ nodeapp/routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+  //Utilizar variables globales para renderizar las variables de la vista
+  //res.render('index'); 
+
+  //Utilizar objeto para pasar las variables
+  let ahora=new Date();
+  let segundos=ahora.getSeconds();
+  
+  //Guardar las variables en la respuesta
+  //res.locals.nombre="Ivan"
+  res.locals.bienvenido="Bienvenido";
+  res.locals.codigo='<script>alert("inyección de código")</script>';
+  res.locals.usuarios=[
+    {nombre:'Jones', edad:45},
+    {nombre: 'Smith', edad:34}
+  ];
+
+  //Llamada al renderizado
+  res.render('index', { color: 'rojo', segundoActual:segundos, condicion:segundos %2===0 });
+
 });
 
 module.exports = router;
